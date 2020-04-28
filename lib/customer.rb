21e17c07f1395f7b0e884cf8086e723448b1bf9d
William Mittard <billymittard@ymail.com>
2020-04-28 22:03:34 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,21 @@
 class Customer
+  attr_accessor :meals
+  attr_reader :name, :age 
+  
+  @@all = []
+  
+  def initialize(name,age)
+    @name = name 
+    @age = age 
+    @@all << self
+  end 
+    
+    def self.all 
+      @@all 
+    end 
+    
+    def meals
+      @meals = []
+    end
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
+attr_reader :name, :years_of_experience
+
+@@all = []
+
+def initialize(name,years_of_experience)
+  @name = name 
+  @years_of_experience = years_of_experience
+  @@all << self
+end 
+
+def self.all 
+  @@all 
+end 
 
 end
