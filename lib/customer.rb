dc9ef90fbad6c9779fd4774a03e7df1d952d1c82
William Mittard <billymittard@ymail.com>
2020-04-28 22:49:01 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
 class Customer
+  attr_accessor :meals
+  attr_reader :name, :age 
   
+  @@all = []
+  
+  def initialize(name,age)
+    @name = name 
+    @age = age 
+    @@all << self
+  end 
+    
+    def self.all 
+      @@all 
+    end 
+    
+    def new_meal(waiter,total,tip=0) 
+      Meal.new(waiter,self,total,tip)
+      #we don't need to take customer in as an arguement as we are passing self as a reference to the current instance of customer.
+    end 
+      
+  def meals 
+    Meal.all.select do |meal| 
+      meal.customer == self 
+      # we are iterating through every instance of Meal and returning only the ones where the meal"s customer matches the current customer.
+    end 
+  end 
+  
+  def waiters 
+    meals.map do |meal| 
+      meal.waiter
+    end 
+  end 
 end
